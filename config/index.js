edca2c878db40f5e018782da1601a9142574c4b0
williamsriver <994975860@qq.com>
2020-09-20 08:58:48 +0800

    correct port

--- config/index.js
+++ config/index.js
@@ -19,7 +19,7 @@ module.exports = {
         }
       },
       '/api':{
-        target:'http://tonycoder.ziqiang.net.cn',//请求域名
+        target:'http://tonycoder.ziqiang.net.cn:8080/',//请求域名
         //secure: false, // 如果是https接口，需要配置这个参数
         changeOrigin:true,//如果是跨域访问，需要配置这个参数
         pathRewrite:{
